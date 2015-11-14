d62a0101fba2cf7cbeb158093b8757d8dd24c260
jgreene2k <jgreene2k@hotmail.com>
2015-11-14 17:33:26 -0500

    Created ReservationStatus static strings Edited FrontDeskMenu: onActionCancel

---
+++ src/hms/model/ReservationStatus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hms.model;
+
+/**
+ *
+ * @author jgreene
+ */
+public class ReservationStatus {
+    
+    public static String PENDING = "PENDING";
+    public static String CHECKEDIN = "CHECKEDIN";
+    public static String CHECKEDOUT = "CHECKEDOUT";
+    public static String CANCELLED = "CANCELLED";
+
+}
