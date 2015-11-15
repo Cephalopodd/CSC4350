98acb6fcd62ddf1c8fdcd7be6b5c2022262609c4
Fenil <Fenil@Fenil-Desktop>
2015-11-15 18:18:45 -0500

    Created Pie Chart and verified it was working.
    Added the getters in the Admin DAO to get check in and check out data.

--- src/hms/AdminMenuController.java
+++ src/hms/AdminMenuController.java
@@ -4,31 +4,38 @@ import hms.model.AdminDAO;
 import hms.model.User;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author Fenil
- */
 public class AdminMenuController implements Initializable, SubMenu {
     
     AdminDAO dao;
     double totalSales;
+    int checkOutToday;
+    int checkInToday;
 
     @FXML
     private Label txtTotalSales;
+    @FXML
+    private PieChart checkStatsPieChart;
 
-    /**
-     * Initializes the controller class.
-     */
+    //Initializes the controller class.
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
         dao = new AdminDAO();
         setTotalSales();
+        
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+        new PieChart.Data("Check Outs", dao.getCheckOut(checkOutToday)),
+        new PieChart.Data("Check In", dao.getCheckIn(checkInToday))); 
+        
+        checkStatsPieChart.setTitle("Check In Stats");
+        checkStatsPieChart.setData(pieChartData);
     }    
 
     @Override
@@ -41,6 +48,7 @@ public class AdminMenuController implements Initializable, SubMenu {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    //Method that calls DAO to get total sales of the day to display in the txtTotalSales label.
     private void setTotalSales(){
         String daoTotalSales = Double.toString(dao.getTotalSales(totalSales));
         txtTotalSales.setText("$" + daoTotalSales);
--- src/hms/model/AdminDAO.java
+++ src/hms/model/AdminDAO.java
@@ -1,18 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hms.model;
-
 
 public class AdminDAO {
     
     //Query that accesses the database and get toatalSales of the current day.
+    //Ex. If the total sales is 123.33 for %Todays Date%, query will return 123.33.
+    //Let me know if it is easier to return string vs double or int.
     public double getTotalSales(double totalSales) {
         //Please write query to return the total Sales for the current day.
         double sales = 123.23;
         return sales;
     }
     
+    //Query that returns the an int value of check outs that are schedule for the day.
+    //Ex. If there are 10 check out schedules for %Todays Date%, the query will return a value of 10.
+    public int getCheckOut(int checkOutToday){
+        checkOutToday = 5;
+        return checkOutToday;
+    }
+    
+    //Query that returns the an int value of check ins that are schedule for the day.
+    //Ex. If there are 10 check in schedules for %Todays Date%, the query will return a value of 10.
+    public int getCheckIn(int checkInToday){
+        checkInToday = 20;
+        return checkInToday;
+    }
 }
