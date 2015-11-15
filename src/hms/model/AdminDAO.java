e3b0ba83cd10ca06e8f7b966c22151247d0e00c9
Fenil <Fenil@Fenil-Desktop>
2015-11-15 17:33:24 -0500

    Updated Amin portal.
    Added AdminDAO

---
+++ src/hms/model/AdminDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hms.model;
+
+
+public class AdminDAO {
+    
+    //Query that accesses the database and get toatalSales of the current day.
+    public String getTotalSales(String totalSales) {
+        //String sales = "123.23";
+        return sales;
+    }
+    
+}
