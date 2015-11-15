9aac214c22ea81b0e9ae03ff9cbb53c01119e242
Fenil <Fenil@Fenil-Desktop>
2015-11-15 18:47:52 -0500

    Added line chart to the admin portal.
    Added fake data for days of the week.
    
    Will clarify and add the getter in the Admin DAO.

--- src/hms/AdminMenuController.java
+++ src/hms/AdminMenuController.java
@@ -8,7 +8,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 public class AdminMenuController implements Initializable, SubMenu {
@@ -22,6 +24,8 @@ public class AdminMenuController implements Initializable, SubMenu {
     private Label txtTotalSales;
     @FXML
     private PieChart checkStatsPieChart;
+    @FXML
+    private LineChart<?, ?> weeklySales;
 
     //Initializes the controller class.
     @Override
@@ -30,12 +34,30 @@ public class AdminMenuController implements Initializable, SubMenu {
         dao = new AdminDAO();
         setTotalSales();
         
+        //Sets the Pie Chart Title.
+        checkStatsPieChart.setTitle("Check In Stats");
+        
+        //Adds the data to the pie chart.
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
         new PieChart.Data("Check Outs", dao.getCheckOut(checkOutToday)),
-        new PieChart.Data("Check In", dao.getCheckIn(checkInToday))); 
-        
-        checkStatsPieChart.setTitle("Check In Stats");
+        new PieChart.Data("Check In", dao.getCheckIn(checkInToday)));
         checkStatsPieChart.setData(pieChartData);
+        
+        //Sets the name of the Line Chart.
+        weeklySales.setTitle("Weekly Sales");
+        
+        //Defines series for the line chart.
+        XYChart.Series currentWeek = new XYChart.Series();
+        currentWeek.setName("Day of the Week");
+        currentWeek.getData().add(new XYChart.Data("Monday", 230.00));
+        currentWeek.getData().add(new XYChart.Data("Tuesday", 141.00));
+        currentWeek.getData().add(new XYChart.Data("Wednesday", 152.00));
+        currentWeek.getData().add(new XYChart.Data("Thursday", 248.23));
+        currentWeek.getData().add(new XYChart.Data("Friday", 3400.33));
+        currentWeek.getData().add(new XYChart.Data("Saturday", 3615.22));
+        currentWeek.getData().add(new XYChart.Data("Sunday", 455.00));
+        
+        weeklySales.getData().add(currentWeek);
     }    
 
     @Override
