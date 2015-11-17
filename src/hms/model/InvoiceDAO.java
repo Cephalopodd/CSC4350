c87b3588944000a7ce2097556f22debca285ca70
rkentmc <rmcarthur1@student.gsu.edu>
2015-11-17 00:50:00 -0500

    set up the billing menu

---
+++ src/hms/model/InvoiceDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hms.model;
+
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author admin
+ */
+public class InvoiceDAO {
+
+    public InvoiceDAO() {
+    }
+
+    public ObservableList<FolioCharge> queryCharges(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //Needs query to link all data fields to print the final invoice
+    
+    
+}
